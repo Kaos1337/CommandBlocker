150db8a4fbedf6bf6f2a531f16921a548a1e2b46
Francesco Marretta <francescomsg@gmail.com>
2012-08-21 16:33:04 +0200

    Added metrics

--- src/net/kaoslabs/commandblocker/CommandBlocker.java
+++ src/net/kaoslabs/commandblocker/CommandBlocker.java
@@ -1,15 +1,33 @@
 package net.kaoslabs.commandblocker;
 
-import net.kaoslabs.commandblocker.util.ConfigHelper;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.kaoslabs.commandblocker.util.ConfigHelper;
+import net.kaoslabs.commandblocker.util.Metrics;
+
+
 public class CommandBlocker extends JavaPlugin {
 
+	public static final Logger log = Logger.getLogger("Minecraft");
 
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
+		// start Metrics
+		startMetrics();
+	}
+	
+	public void startMetrics() { 	
+		try {	
+			Metrics metrics = new Metrics(this);	
+			metrics.start();
+			CommandBlocker.log.info("[CommandBlocker] Metrics connection started.");
+		} catch (IOException e) {
+			CommandBlocker.log.warning("[CommandBlocker] Failed to submit the stats :-("); // Failed to submit the stats :-(
+		}
 	}
 	
 	/**
